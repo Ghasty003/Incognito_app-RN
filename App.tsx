ad0cf5fba68a8ca6c95d797678c765e3cfe172d0
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-29 14:55:37 -0300

    init react native paper

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import  React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { Provider } from "react-native-paper";
+
 import SplashScreen from './src/screens/SplashScreen';
 import Login from './src/screens/Login';
 import { StackParams } from './src/utils/types';
@@ -13,15 +15,17 @@ const Stack = createNativeStackNavigator<StackParams>();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='SplashScreen' screenOptions={{
-        headerStyle: {
-          backgroundColor: "dodgerblue"
-        }
-      }}>
-        <Stack.Screen name='SplashScreen' component={SplashScreen} />
-        <Stack.Screen name='Login' component={Login} />
-      </Stack.Navigator>
-      <StatusBar style="light" />
+      <Provider>
+        <Stack.Navigator initialRouteName='SplashScreen' screenOptions={{
+          headerStyle: {
+            backgroundColor: "dodgerblue"
+          }
+        }}>
+          <Stack.Screen name='SplashScreen' component={SplashScreen} />
+          <Stack.Screen name='Login' component={Login} />
+        </Stack.Navigator>
+        <StatusBar style="light" />
+      </Provider>
     </NavigationContainer>
   );
 }
