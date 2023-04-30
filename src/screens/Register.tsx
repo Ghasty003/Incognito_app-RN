ddc49b8174212d5ccbf0fd31762ab161fce224c3
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-30 01:29:30 -0300

    setup registration

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Icon from "react-native-vector-icons/Ionicons";
 import { View, Text, StyleSheet, TouchableWithoutFeedback, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native-paper";
@@ -7,13 +7,22 @@ import { LoginProp, RegisterProp } from '../utils/types';
 import colors from '../utils/colors';
 import { Image } from 'react-native';
 import useToggle from '../hooks/useToggle';
+import API from '../utils/api';
 
 function Login({ navigation, route }: RegisterProp) {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     const usernameRef = useRef<any>(null!);
     const passwordRef = useRef<any>(null!);
 
     const [value, toggleValue] = useToggle(true);
+
+    async function handleRegister() {
+        const data = await API.post("auth/signup", { username, password });
+        console.log(data)
+    }
 
     React.useEffect(() => {
         navigation.setOptions({
@@ -37,14 +46,14 @@ function Login({ navigation, route }: RegisterProp) {
                         <Text style={styles.headerText}>Register</Text>
                     </View>
 
-                    <TextInput style={styles.input} mode='flat' ref={usernameRef}
-                    label="Username" right={<TextInput.Icon icon={() => <Icon name='person' size={20} />} />} />
+                    <TextInput value={username} onChangeText={setUsername} style={styles.input} mode='flat' ref={usernameRef}
+                        label="Username" right={<TextInput.Icon icon={() => <Icon name='person' size={20} />} />} />
 
-                    <TextInput style={styles.input} mode='flat' secureTextEntry={value} ref={passwordRef}
+                    <TextInput value={password} onChangeText={setPassword} style={styles.input} mode='flat' secureTextEntry={value} ref={passwordRef}
                         label="Password" right={<TextInput.Icon
                         icon={() => <Icon onPress={toggleValue} name={value ? "eye" : "eye-off"} size={20} />} />} />
 
-                    <TouchableOpacity style={styles.loginContainer}>
+                    <TouchableOpacity onPress={handleRegister} style={styles.loginContainer}>
                         <Text style={styles.login}>Register</Text>
                     </TouchableOpacity>
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,47 @@
-async function API (endpoint: string, method: "GET" | "POST", values: object) {
-    const res = await fetch(`https://incognito-j4hs.onrender.com/${endpoint}`, {
-        method,
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(values)
-    });
+class API {
+
+    public constructor() {
+
+    }
+
+    public async get(endpoint = "") {
+        try {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users/" + endpoint);
+            const json = await res.json();
+
+            return json;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    public async post(endpoint = "", data: object) {
+        try {
+
+            const res = await fetch("https://incognito-j4hs.onrender.com/" + endpoint, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+        
+            const json = await res.json();
+
+            if (res.ok) {
+                return json;
+            } else {
+                return json.message;
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 
+const api = new API();
 
-export default API;
+
+export default api;
