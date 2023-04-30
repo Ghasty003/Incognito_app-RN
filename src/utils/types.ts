d20cc02d1f58e7ab47a537553f97c32aa5a7b5cd
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-29 22:09:49 -0300

    added register screen prop

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,9 +2,12 @@ import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 export type StackParams = {
     SplashScreen: undefined;
-    Login: undefined
+    Login: undefined;
+    Register: undefined;
 }
 
 export type SplashScreenProp = NativeStackScreenProps<StackParams, "SplashScreen">;
 
 export type LoginProp = NativeStackScreenProps<StackParams, "Login">;
+
+export type RegisterProp = NativeStackScreenProps<StackParams, "Register">;
