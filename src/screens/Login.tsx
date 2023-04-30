3cd0fd1e8b6f45f136db4778944089ea7344afd4
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-29 21:51:08 -0300

    updated login layout design

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,36 +1,62 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Icon from "react-native-vector-icons/Ionicons";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableWithoutFeedback, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native-paper";
 
 import { LoginProp } from '../utils/types';
 import colors from '../utils/colors';
 import { Image } from 'react-native';
+import useToggle from '../hooks/useToggle';
 
 function Login({ navigation, route }: LoginProp) {
+
+    const usernameRef = useRef<any>(null!);
+    const passwordRef = useRef<any>(null!);
+
+    const [value, toggleValue] = useToggle(true);
 
     React.useEffect(() => {
         navigation.setOptions({
             headerTintColor: "white"
         })
     }, []);
+
+    const handleContainerPress = () => {
+        usernameRef.current.blur();
+        passwordRef.current.blur();
+    }
+
     
     return (
-        <View style={styles.container}>
-            <View style={styles.authContainer}>
+        <TouchableWithoutFeedback onPress={handleContainerPress}>
+            <View style={styles.container}>
+                <View style={styles.authContainer}>
 
-                <View style={styles.header}>
-                    <Image style={styles.headerImg} source={require("../../assets/anonymous-message.png")} />
-                    <Text style={styles.headerText}>Login</Text>
+                    <View style={styles.header}>
+                        <Image style={styles.headerImg} source={require("../../assets/anonymous-message.png")} />
+                        <Text style={styles.headerText}>Login</Text>
+                    </View>
+
+                    <TextInput style={styles.input} mode='flat' ref={usernameRef}
+                    label="Username" right={<TextInput.Icon icon={() => <Icon name='person' size={20} />} />} />
+
+                    <TextInput style={styles.input} mode='flat' secureTextEntry={value} ref={passwordRef}
+                        label="Password" right={<TextInput.Icon
+                        icon={() => <Icon onPress={toggleValue} name={value ? "eye" : "eye-off"} size={20} />} />} />
+
+                    <TouchableOpacity style={styles.loginContainer}>
+                        <Text style={styles.login}>Login</Text>
+                    </TouchableOpacity>
+
+                    <View style={styles.registerContainer}>
+                        <Text style={{color: "white", fontSize: 16}}>Already have an account?</Text>
+                        <TouchableOpacity>
+                            <Text style={{color: colors.dodger, fontSize: 16}}>Register</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-
-                <TextInput style={styles.input} mode='flat'
-                 label="Username" right={<TextInput.Icon icon={() => <Icon name='person' size={20} />} />} />
-
-                <TextInput style={styles.input} mode='flat' secureTextEntry={true}
-                 label="Password" right={<TextInput.Icon icon={() => <Icon name='lock-closed' size={20} />} />} />
             </View>
-        </View>
+        </TouchableWithoutFeedback>
     );
 }
 
@@ -40,14 +66,16 @@ export default Login;
 const styles = StyleSheet.create({
     authContainer: {
         backgroundColor: colors.navbg,
-        width: "80%",
-        alignItems: "center"
+        width: "85%",
+        alignItems: "center",
+        padding: 15,
+        borderRadius: 8
     },
     container: {
         flex: 1,
         backgroundColor: colors.primary,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
     },
     header: {
         flexDirection: "row",
@@ -66,6 +94,21 @@ const styles = StyleSheet.create({
     },
     input: {
         width: "80%",
-        marginVertical: 10
+        marginVertical: 10,
+    },
+    loginContainer: {
+        backgroundColor: "tomato",
+        paddingHorizontal: 35,
+        paddingVertical: 10,
+        borderRadius: 8,
+        marginTop: 20
+    },
+    login: {
+        color: "white"
+    },
+    registerContainer: {
+        marginTop: 30,
+        flexDirection: "row",
+        gap: 8
     }
 })
