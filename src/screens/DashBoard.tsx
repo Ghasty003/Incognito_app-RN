2f9ca6d6bff3534339c0056eb87af602dd9dd668
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-04-29 22:31:58 -0300

    init dashboard screen

---
+++ src/screens/DashBoard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function DashBoard() {
+    return (
+        <View>
+            <Text>Hello DashBoard</Text>
+        </View>
+    );
+}
+
+export default DashBoard;
